11976c2dc86ed2926debe657378e12e48130f82a
Arthur Chang <arthurc0102@gmail.com>
2019-11-07 14:17:47 +0800

    feat(links): add viewsets

--- src/app/links/urls.py
+++ src/app/links/urls.py
@@ -13,17 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 
-from . import versions
+from rest_framework.routers import SimpleRouter
+
+from .views import LinkViewSet
 
 
-api_urlpatterns = [
-    path('links/', include('app.links.urls')),
-]
+app_name = 'links'
+
+router = SimpleRouter(False)
+router.register('links', LinkViewSet)
 
 urlpatterns = [
-    path('v1/', include((api_urlpatterns, versions.v1))),
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]

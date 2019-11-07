5b5df68e8bd152c4fdfdeaae525f4170e89a9aac
Arthur Chang <arthurc0102@gmail.com>
2019-11-07 19:12:30 +0800

    improvement(links): normal user can only modify self links

--- src/app/links/views.py
+++ src/app/links/views.py
@@ -11,6 +11,13 @@ class LinkViewSet(ModelViewSet):
     queryset = Link.objects.order_by('id')
     serializer_class = LinkSerializer
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.is_staff:
+            queryset = queryset.filter(creator=self.request.user)
+
+        return queryset
+
     def perform_create(self, serializer):
         # Handle unique together, because creator is not required in serializer
         try:
